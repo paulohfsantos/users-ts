d2284497f4113ebb43720992f6177962cfda8579
Paulo Santos <paulohfsantos1@gmail.com>
2021-12-29 18:23:58 -0300

    update, delete, get by id

--- src/controllers/userController.ts
+++ src/controllers/userController.ts
@@ -1,6 +1,26 @@
 import { Request, Response } from 'express';
 import { User } from '../models/User';
 
+export const users = async (req: Request, res: Response) => {
+  return await User.findAll()
+    .then(result => res.status(200).json(result))
+}
+
+export const user = async (req: Request, res: Response) => {
+  let id = parseInt(req.params.id);
+
+  try {
+    if (id) {
+      await User.findOne({ where: { id } })
+        .then(result => res.status(200).json(result))
+    }
+  } catch (err) {
+    res.status(500).json({
+      message: 'internal server error',
+    })
+  }
+}
+
 export const newUser = async (req: Request, res: Response) => {
   let name: string = req.body.name;
   let age: number = req.body.age;
@@ -19,14 +39,12 @@ export const newUser = async (req: Request, res: Response) => {
 
     if (name && age) {
       await User.create({ name, age })
-        .then((result) => {
-          console.log(result);
+        .then(() => {
           res.status(200).json({
             message: 'user created',
           })
         })
-        .catch(err => {
-          console.log('erro aqui => ', err);
+        .catch(() => {
           res.status(500).json({
             message: 'user has not been created',
           })
@@ -39,7 +57,37 @@ export const newUser = async (req: Request, res: Response) => {
   }
 }
 
-export const users = async (req: Request, res: Response) => {
-  return await User.findAll()
-    .then(result => res.status(200).json(result))
+export const updateUser = async (req: Request, res: Response) => {
+  let id = parseInt(req.params.id);
+  let { name, age } = req.body;
+
+  try {
+    if (id) {
+      await User.update({ name, age }, { where: { id } })
+      res.status(200).json({
+        message: 'user updated',
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      message: 'internal server error',
+    });
+  }
+}
+
+export const deleteUser = async (req: Request, res: Response) => {
+  let id = parseInt(req.params.id);
+
+  try {
+    if (id) {
+      await User.destroy({ where: { id } })
+      res.status(200).json({
+        message: 'user deleted',
+      });
+    }
+  } catch (error) {
+    return res.status(500).json({
+      message: 'internal server error',
+    });
+  }
 }
--- src/routes/index.ts
+++ src/routes/index.ts
@@ -4,9 +4,14 @@ import * as UserController from '../controllers/userController'
 
 const route = Router()
 
+// home controller
 route.get('/', HomeController.home)
 route.get('/ping', HomeController.ping)
+// user controller
 route.get('/users', UserController.users)
+route.get('/user/:id', UserController.user)
 route.post('/user', UserController.newUser)
+route.put('/user/:id', UserController.updateUser)
+route.delete('/user/:id', UserController.deleteUser)
 
 export default route
