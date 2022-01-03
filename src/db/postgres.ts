58a5c4d93e5c5e0a24970066483067ebf803104b
Paulo Santos <paulohfsantos1@gmail.com>
2022-01-03 01:58:58 -0300

    update postgres db url and package.json

--- src/db/postgres.ts
+++ src/db/postgres.ts
@@ -4,11 +4,16 @@ import dotenv from 'dotenv'
 dotenv.config()
 
 export const sequelize = new Sequelize(
-  process.env.PG_DB as string,
+  process.env.PG_DB || process.env.DATABASE_URL as string,
   process.env.PG_USER as string,
   process.env.PG_PASSWORD as string,
   {
     dialect: 'postgres',
     port: parseInt(process.env.PG_HOST as string),
+    protocol: 'postgres',
+    dialectOptions: {
+      ssl: true,
+      rejectUnauthorized: false,
+    }
   }
 )
